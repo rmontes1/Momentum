a1c576f3e809a1f8a63e69c2451799d96250b7c7
rmontes1 <rmontes1@ucsc.edu>
2014-07-10 14:58:27 -0700

    Pushed InControl Assets , as well as beginning GameManager

---
+++ Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+//setup gamepad support
+//needs to be checking when controllers are plugged in
+//keep track of what player is using what controller
+
+public class ControllerManager : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		InputManager.Setup ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputManager.Update ();
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent (typeof(ControllerManager))]
+public class GameManager : MonoBehaviour {
+
+	public static GameManager gameManager;
+	public ControllerManager controllerManager;
+
+	void Awake(){
+		if( gameManager == null ){
+
+			DontDestroyOnLoad(gameObject);
+			gameManager = this;
+			Init();
+		}
+		else if(gameManager != this ){
+			Destroy(gameObject);
+		}
+
+	}
+
+	//setup gamemanager references
+	void Init(){
+		controllerManager = this.GetComponent<ControllerManager> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
